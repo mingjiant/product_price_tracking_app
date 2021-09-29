546b1f8746baf07ee6476d72c6ce3d1bdf7371bb
Ming Jian <18085126@imail.sunway.edu.my>
2021-09-29 21:14:13 +0800

    Update retailer list

--- lib/retailer_list.dart
+++ lib/retailer_list.dart
@@ -1,8 +1,15 @@
 import './models/retailer.dart';
 
 final retailerList = [
-  Retailer(id: '1', name: 'Jaya Grocer'),
-  Retailer(id: '2', name: 'Village Grocer'),
-  Retailer(id: '3', name: 'Tesco'),
-  Retailer(id: '4', name: 'Giant'),
+  Retailer(id: '1', name: 'AEON'),
+  Retailer(id: '2', name: 'Caring'),
+  Retailer(id: '3', name: 'Giant'),
+  Retailer(id: '4', name: 'Jaya Grocer'),
+  Retailer(id: '5', name: 'Lotus (Tesco)'),
+  Retailer(id: '6', name: 'MyNews'),
+  Retailer(id: '7', name: 'SC Neighbourhood Market'),
+  Retailer(id: '8', name: 'Watsons)'),
+  Retailer(id: '9', name: 'Village Grocer'),
+  Retailer(id: '10', name: '7Eleven'),
+  Retailer(id: '11', name: '99 Speedmart'),
 ];
